16a0000e0f75415370d0a3e3d8a52e692648dd6e
ChanYvonne <ychan1352@gmail.com>
2021-05-11 19:40:42 -0400

    added search by song and artist

--- callSpotify.py
+++ callSpotify.py
@@ -3,123 +3,65 @@ import json
 # To access authorised Spotify data
 from spotipy.oauth2 import SpotifyClientCredentials
 
-client_id = '{client_id}'
-client_secret = '{client_secret}'
+client_id = 'f0338d1fd79f4310835cede197372d6'
+client_secret = '1e395ecaf45f488fa539560d57963bf6'
 
 client_credentials_manager = SpotifyClientCredentials(
     client_id=client_id, client_secret=client_secret)
 # spotify object to access APIBQDAPTRBDFmfEX0bq0z_uXuBatyQ6EDJvbg
-sp = spotipy.Spotify(auth='{Auth_token}',
+sp = spotipy.Spotify(auth='BQBGZANA7n2ClhCZrupwbsY3py8CfSwqHeaYpgQ8cHB7_EfPD0bBvLI15saUQUh7oRV95kHLfUOJAILnFs0cMh_kW4F5n8YTTcG2LVVjtqF6dP4pYp78hy7Jf8l7aUugADIdpgrYBMnoOQ',
                      client_credentials_manager=client_credentials_manager)
-name = "Walk the Moon"  # chosen artist
-result = sp.search(name)  # search query
-# print(result['tracks']['items'][0]['artists'])
-
-# Extract Artist's uri
-artist_uri = result['tracks']['items'][0]['artists'][0]['uri']
-
-# Pull all of the artist's albums
-sp_albums = sp.artist_albums(artist_uri, album_type='album')
-# Store artist's albums' names' and uris in separate lists
-album_names = []
-album_uris = []
-# for i in range(len(sp_albums['items'])):
-for i in range(2):
-    album_names.append(sp_albums['items'][i]['name'])
-    album_uris.append(sp_albums['items'][i]['uri'])
-
-print(album_names)
-# print(album_uris)
-# Keep names and uris in same order to keep track of duplicate albums
-
-spotify_albums = {}
 
 
-def album_songs(uri, alb_count):
-    album = uri  # assign album uri to a_name
-
-    spotify_albums[album] = {}  # Creates dictionary for that specific album
-    # Create keys-values of empty lists inside nested dictionary for album
-    spotify_albums[album]['album'] = []  # create empty list
-    spotify_albums[album]['track_number'] = []
-    spotify_albums[album]['id'] = []
-    spotify_albums[album]['name'] = []
-    spotify_albums[album]['uri'] = []
-
-    tracks = sp.album_tracks(album)  # pull data on album tracks
-    # print(tracks)
-
-    for n in range(len(tracks['items'])):  # for each song track
-        # append album name tracked via album_count
-        spotify_albums[album]['album'].append(album_names[alb_count])
-        spotify_albums[album]['track_number'].append(
-            tracks['items'][n]['track_number'])
-        spotify_albums[album]['id'].append(tracks['items'][n]['id'])
-        spotify_albums[album]['name'].append(tracks['items'][n]['name'])
-        spotify_albums[album]['uri'].append(tracks['items'][n]['uri'])
+def get_track_uri(track, artist):
+    result = sp.search(track + " " + artist, limit=10, type='track,artist')
+    track_uri = result['tracks']['items'][0]['uri']
+    # print(track_uri)
+    # maybe helpful later
+    popularity = result['tracks']['items'][0]['popularity']
+    return track_uri
+    # print(json.dumps(result, indent=4))
 
 
-def audio_features(album):
-    # Add new key-values to store audio features
-    spotify_albums[album]['acousticness'] = []
-    spotify_albums[album]['danceability'] = []
-    spotify_albums[album]['energy'] = []
-    spotify_albums[album]['instrumentalness'] = []
-    spotify_albums[album]['liveness'] = []
-    spotify_albums[album]['loudness'] = []
-    spotify_albums[album]['speechiness'] = []
-    spotify_albums[album]['tempo'] = []
-    spotify_albums[album]['valence'] = []
-    spotify_albums[album]['popularity'] = []
+def get_audio_features(track_uri):
 
-    trackIds = []
-    for track in spotify_albums[album]['id']:
-        trackIds.append(str(track))
+    track_info = {}
 
-    # print(trackIds)
-    # pull audio features per track
-    features = sp.audio_features(trackIds)
-    # print(features)
-    print(json.dumps(features, indent=4))
+    features = sp.audio_features(track_uri)
+    # print(json.dumps(features, indent=4))
 
-    # analysis = sp.audio_analysis(trackIds[0])
-    # print(analysis)
+    track_info['acousticness'] = features[0]['acousticness']
+    track_info['danceability'] = features[0]['danceability']
+    track_info['energy'] = features[0]['energy']
+    track_info['instrumentalness'] = features[0]['instrumentalness']
+    track_info['liveness'] = features[0]['liveness']
+    track_info['loudness'] = features[0]['loudness']
+    track_info['speechiness'] = features[0]['speechiness']
+    track_info['tempo'] = features[0]['tempo']
+    track_info['valence'] = features[0]['valence']
 
-    # Append to relevant key-value
-    if not(all(x is None for x in features)):
-        spotify_albums[album]['acousticness'].append(
-            features[0]['acousticness'])
-        spotify_albums[album]['danceability'].append(
-            features[0]['danceability'])
-        spotify_albums[album]['energy'].append(features[0]['energy'])
-        spotify_albums[album]['instrumentalness'].append(
-            features[0]['instrumentalness'])
-        spotify_albums[album]['liveness'].append(features[0]['liveness'])
-        spotify_albums[album]['loudness'].append(features[0]['loudness'])
-        spotify_albums[album]['speechiness'].append(features[0]['speechiness'])
-        spotify_albums[album]['tempo'].append(features[0]['tempo'])
-        spotify_albums[album]['valence'].append(features[0]['valence'])
+    return track_info
 
-    # popularity is stored elsewhere
-    pop = sp.track(track)
-    spotify_albums[album]['popularity'].append(pop['popularity'])
-    print(pop)
 
-    # print(spotify_albums[album])
+def get_audio_analysis(track_uri):
+    analysis = sp.audio_analysis(track_uri)
+    # print(json.dumps(analysis, indent=4))
+    sections = analysis['sections']
+    segments = analysis['segments']
+
+    print(json.dumps(sections, indent=4))
+    # print(len(sections))
+    print(len(segments))
 
 
 def main():
-    album_count = 0
-    for i in album_uris:  # each album
-        album_songs(i, album_count)
-        print("Album " + str(album_names[album_count]) +
-              " songs has been added to spotify_albums dictionary")
-        album_count += 1  # Updates album count once all tracks have been added
+    # Process is getting albums from artist, getting songs from album, then getting the song
 
-    for i in spotify_albums:
-        audio_features(i)
-
-    # print(spotify_albums)
+    artist = "Taylor Swift"  # chosen artist
+    track = "Mr. Perfectly Fine"
+    track_uri = get_track_uri(track, artist)
+    track_info = get_audio_features(str(track_uri))
+    get_audio_analysis(track_uri)
 
 
 if __name__ == "__main__":
